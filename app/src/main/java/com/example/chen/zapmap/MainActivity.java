78f045a92323f6ad367b0a7911aa1bdda78d8d1f
chenbok <chenbar@post.bgu.ac.il>
2016-07-23 13:50:30 +0300

    create main activity basic layout: appBar+tabsLayout

--- app/src/main/java/com/example/chen/zapmap/MainActivity.java
+++ app/src/main/java/com/example/chen/zapmap/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.chen.zapmap;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        // set the toolbar as the app bar for the activity
+//        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.app_name);
+        }
+//        actionBar.hide();
     }
 }
